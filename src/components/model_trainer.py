86f570be158c84f162fba9f23ce7d122cf3d11f9
JyotsnaAmbekar <jyotsnakunte1921@gmail.com>
2023-06-23 21:27:08 -0500

    Adding pipelines

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -18,20 +18,16 @@ from src.exception import CustomException
 from src.logger import logging
 
 from src.utils import save_object,evaluate_models
-logging.info(f"Importing is done")
 
 @dataclass
 class ModelTrainerConfig:
     trained_model_file_path=os.path.join("artifacts","model.pkl")
-logging.info(f"one class thing done")
 
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
 
-    logging.info(f"second class thing")
     def initiate_model_trainer(self,train_array,test_array):
-        logging.info(f"next is try block")
         try:
             logging.info("Split training and test input data")
             X_train,y_train,X_test,y_test=(
@@ -90,10 +86,8 @@ class ModelTrainer:
             logging.info(f"Parameters given")
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
                                              models=models,param=params)
-            logging.info(f"Model report step is done")
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
-            logging.info(f"best score calculated")
             ## To get best model name from dict
 
             best_model_name = list(model_report.keys())[
@@ -104,16 +98,13 @@ class ModelTrainer:
             if best_model_score<0.6:
                 raise CustomException("No best model found")
             logging.info(f"Best found model on both training and testing dataset")
-            logging.info(f"step before saving object")
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
             )
-            logging.info(f"saving as object")
             predicted=best_model.predict(X_test)
 
             r2_square = r2_score(y_test, predicted)
-            logging.info(f"r2 calculated")
             return r2_square
         except Exception as e:
             raise CustomException(e,sys)
